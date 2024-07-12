9e62ccd8356ab6ef2c36514db2b423de52811289
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-07-12 15:56:26 -0300

    carregando imagens pelo js

---
+++ js/app.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+    cardapio.eventos.init();
+
+
+
+});
+
+var cardapio = {};
+
+cardapio.eventos = {
+
+    init: () => {
+        cardapio.metodos.obterItensCardapio();
+    }
+
+}
+
+cardapio.metodos = {
+//obtem a lista de itens do cardápio
+    obterItensCardapio: (categoria = 'burgers') => {
+
+        var filtro = MENU[categoria];
+        console.log(filtro);
+
+        $("#itensCardapio").html('');
+
+        $.each(filtro, (i,e) => {
+
+            console.log(e.name);
+
+            let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
+            .replace(/\${nome}/g, e.name)
+            .replace(/\${preco}/g, e.price.toFixed(2).replace('.',','));
+
+            $("#itensCardapio").append(temp);
+
+        });
+
+        //remove o ativo
+        $(".container.menu a").remoceClass('active');
+
+        //seta o menu para ativo
+
+        $("#menu-"+ categoria).addClass('active');
+
+    }
+}
+
+cardapio.templates = {
+
+    item: `
+           <div class="col-3 mb-5">
+                            <div class="card card-item">
+                                <div class="img-produto">
+                                    <img src="\${img}"/>
+                                </div>
+                                <p class="title-produto text-center mt-4">
+                                <b>\${nome}</b>
+                                </p>
+
+                                <p class="price-produto text-center">
+                                <b>R$ \${preco}</b>
+                                </p>
+
+                                <div class="add-carrinho">
+                                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
+                                    <span class="add-numero-itens">0</span>
+                                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
+                                    <span class="btn-btn-add"><i class="fa fa-shopping-bag"></i></span>
+                                </div>
+                            </div>
+                        </div>
+    
+    `
+}
