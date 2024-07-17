66c88b352acdf71d035884aa305386d5df8531dd
paulorobertorodrigues <https://github.com/paulorobertorodrigues/marketplace.git>
2024-07-17 17:44:44 -0300

    integrando com whatsapp

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ cardapio.eventos = {
 
     init: () => {
         cardapio.metodos.obterItensCardapio();
+        cardapio.metodos.carregarBotaoLigar();
+        cardapio.metodos.carregarBotaoReserva();
     }
 
 }
@@ -510,6 +512,40 @@ cardapio.metodos = {
 
     },
 
+    //carrega o link do botão reserva
+    carregarBotaoReserva: () => {
+
+        var texto = 'Olá! gostaria de fazer uma *reserva*';
+
+        let encode = encodeURI(texto);
+        let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+        $("#btnReserva").attr('href', URL);
+
+    },
+    //carregar botao de ligar
+    carregarBotaoLigar: () => {
+
+        $("#btnLigar").attr('href', `tel:${CELULAR_EMPRESA}`);
+
+    },
+
+    //abre o  depoimento
+    abrirDepoimento: (depoimento) => {
+
+        $("#depoimento-1").addClass('hidden');
+        $("#depoimento-2").addClass('hidden');
+        $("#depoimento-3").addClass('hidden');
+
+        $("#btnDepoimento-1").removeClass('active');
+        $("#btnDepoimento-2").removeClass('active');
+        $("#btnDepoimento-3").removeClass('active');
+
+        $("#depoimento-" + depoimento).removeClass('hidden');
+        $("#btnDepoimento-" + depoimento).addClass('active');
+
+
+    },
 
 
 
